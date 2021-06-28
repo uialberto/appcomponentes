ee86f7b116adbd2313bca0759912663ee2c1515e
Alberto Baigorria <uialberto@gmail.com>
2021-06-28 01:06:22 -0400

    creat input page flutter

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:appcomponentes/src/pages/avatar_page.dart';
 import 'package:appcomponentes/src/pages/home_page.dart';
 import 'package:appcomponentes/src/pages/card_page.dart';
 import 'package:appcomponentes/src/pages/animated_container.dart';
+import 'package:appcomponentes/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
-    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage()
   };
 }
